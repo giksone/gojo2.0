e442c80d12c6e617fdbdd6d8790d82b113d9c40a
giksone <makengogedeon77380@outlook.fr>
2026-01-30 16:13:01 -0500

    juju

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "j'arrive bien maintant a compprendre 🚀"
+    return "j'arrive bien maintant a compprendre!!!!!!!!! 🚀"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
